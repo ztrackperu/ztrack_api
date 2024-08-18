274991a295cc9d090bf6b0af154dadc58db2a354
ztrackperu <ztrack@zgroup.com.pe>
2024-08-17 19:50:26 -0500

    prueba de texto 10

---
+++ app/server/models/comando.py
@@ -0,0 +1,36 @@
+from typing import Optional,List
+from datetime import  datetime
+from pydantic import BaseModel, Field
+
+class ComandoSchema(BaseModel):
+    imei :str = Field(...)
+    estado : Optional[int] |None =1
+    fecha_creacion : Optional[datetime] |None = datetime.now()
+    fecha_ejecucion : Optional[datetime] |None =None
+    comando : str = Field(...)
+    dispositivo : Optional[str] |None = "FAIL"
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "imei":"test01",
+                "estado": 1,
+                "fecha_creacion": "2024-08-17T14:43:11",
+                "comando": "Trama_Readout(3)",
+                "dispositivo": "ZGRU1234567"
+
+            }
+        }
+
+#respuesta cuando todo esta bien
+def ResponseModel(data, message):
+    return {
+        "data": data,
+        "code": 200,
+        "message": message,
+    }
+
+#respuesta cuando algo sale mal 
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
+
---
+++ app/server/routes/comando.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+
+#aqui pedimos las funciones que incluyen nuestro CRUD
+from server.funciones.comando import (
+    GuardarComandos,
+    RetrieveComandos,
+    ProcesarData,
+)
+#Aqui importamos el modelo necesario para la clase 
+from server.models.comando import (
+    ErrorResponseModel,
+    ResponseModel,
+    ComandoSchema,
+    
+)
+router = APIRouter()
+
+@router.post("/", response_description="Datos agregados a la base de datos.")
+async def add_comando(datos: ComandoSchema = Body(...)):
+    datos = jsonable_encoder(datos)   
+    new_notificacion = await GuardarComandos(datos)
+    print("*******")
+    print(new_notificacion)
+    print("*******")
+
+    return new_notificacion
+
+@router.get("/buscar/{imei}", response_description="Datos recuperados")
+async def get_comandos(imei:str):
+    notificacions = await RetrieveComandos(imei)
+    if notificacions:
+        return ResponseModel(notificacions, "Datos  recuperados exitosamente.")
+    return ResponseModel(notificacions, "Lista vacía devuelta")
+
+#secuencua para homologara datos a la plataforma ztrack
+@router.get("/Homologar/", response_description="Datos procesados")
+async def procesar_comandos():
+    notificacions = await ProcesarData()
+    if notificacions:
+        return ResponseModel(notificacions, "Datos  recuperados exitosamente.")
+    return ResponseModel(notificacions, "Lista vacía devuelta xx")
+
