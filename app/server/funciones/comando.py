8801a7ecd8ab2113c1c56f77e5969781fee29481
ztrackperu <ztrack@zgroup.com.pe>
2024-08-22 07:39:24 -0500

    con prueba zgru1234567 3

--- app/server/funciones/comando.py
+++ app/server/funciones/comando.py
@@ -101,14 +101,15 @@ async def ProcesarData():
                     idProgre=id_con
                     
                     tele_dispositivo =14872
-                    if(notificacion['imei']=="868428040102299"):
-                        tele_dispositivo =14874
+
 
                     comparador1 = vali[65] if len(transformado)>65 else 0
                     comparador2 = vali[66] if len(transformado)>66 else 0
-
-
                     valorP = 5 if int(comparador1)==1 else 0
+
+                    if(notificacion['imei']=="868428040102299"):
+                        tele_dispositivo =14874
+                        valorP =  0
 
                     objetoV = {
                             "id": idProgre,
