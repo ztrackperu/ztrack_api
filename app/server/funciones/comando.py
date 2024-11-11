3d5180351d1102a6c9817c82e185620c298c5ed3
ztrackperu <ztrack@zgroup.com.pe>
2024-11-11 10:58:37 -0500

    actualizacion de imeis 3

--- app/server/funciones/comando.py
+++ app/server/funciones/comando.py
@@ -535,7 +535,7 @@ async def ProcesarData():
                             "compress_coil_1": pasar_temp(convertir_a_float(vali[7])),
                             "compress_coil_2": pasar_temp(convertir_a_float(vali[8])), 
                             "ambient_air": pasar_temp(convertir_a_float(vali[9])), 
-                            "cargo_2_temp": pasar_temp(convertir_a_float(vali[10])), 
+                            "cargo_1_temp": pasar_temp(convertir_a_float(vali[10])), 
                             "cargo_2_temp": pasar_temp(convertir_a_float(vali[11])), 
                             "cargo_3_temp": pasar_temp(convertir_a_float(vali[12])), 
                             "cargo_4_temp": pasar_temp(convertir_a_float(vali[13])), 
